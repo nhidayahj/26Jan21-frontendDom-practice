089fa39f61460476ef6ffaa79c76b27e386ba999
Hidayah <hidayah.joha@gmail.com>
2021-01-26 16:01:04 +0800

    ques9-challenges

--- q9_basic_form_manipulation/script.js
+++ q9_basic_form_manipulation/script.js
@@ -1,20 +1,37 @@
-
 function getFormValues() {
+  // Q1. Extract out the email address from the form and store it in a variable named email
+  let email = document.querySelector("#email-address").value;
 
-    let email = null;
+  // Q2. Extract out the selected phone model from the form and store it in a variable named model
+  //Doing linear search
+  let selectedPhone = document.querySelectorAll(".phone-type");
+  let model = null;
+  for (let i of selectedPhone) {
+    if (i.checked) {
+      model = i.value;
+      break;
+    }
+  }
 
-    let model = null;
+  // Q3. Extract out the required services, and store it in a variable named services
+  //since checkboxes, needs to be an array
+  let selectedServices = document.querySelectorAll(".services");
+  let services = [];
+  for (let s of selectedServices) {
+    if (s.checked) {
+      services.push(s.value);
+    }
+  }
 
-    let services = null;
+  // Q4. Extact out the selected pickup point and store it in a variable named pickup
+  let chosenLoc = document.querySelector("#pick-up");
+  let pickup = chosenLoc.value;
 
-    let pickup = null;
-
-    // leave this line alone!
-    return [email, model, services, pickup];
-
+  // leave this line alone!
+  return [email, model, services, pickup];
 }
 
 // LEAVE THE BELOW ALONE! DON'T TOUCH
 try {
-   module.exports = getFormValues;
+  module.exports = getFormValues;
 } catch (e) {}
