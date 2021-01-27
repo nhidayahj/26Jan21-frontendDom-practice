e0b6ef16c7fb09e9b9c03ac5fc096bbebe619a3a
Hidayah <hidayah.joha@gmail.com>
2021-01-27 23:16:44 +0800

    ques5

--- q5_event_handlers/script.js
+++ q5_event_handlers/script.js
@@ -1,3 +1,6 @@
 // PLACE YOUR CODE HERE
 
-
+let changeColorBtn = document.querySelector("#changeColor");
+changeColorBtn.addEventListener("click", function () {
+  document.querySelector("h1").style.backgroundColor = "yellow";
+});
